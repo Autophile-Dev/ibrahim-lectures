096e79276e27f43bee5bb70feb1f2f71c17dc87a
Autophile-Dev <waleed.zaheer55@gmail.com>
2024-07-03 23:46:19 +0800

    Lecture start

--- src/components/form/form1/CreateUser.js
+++ src/components/form/form1/CreateUser.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-
+import './form.css';
 const CreateUser = () => {
   const [userFields, setUserFields] = useState({
     firstName: '',
@@ -18,7 +18,45 @@ const CreateUser = () => {
   const [isLoading, setIsLoading] = useState(false);
   // Uploading data in database
   const handleSubmit = async (e) => {
+    e.preventDefault();
+    let isValid = true;
 
+    const newUserFieldsErrors = { ...userFieldsError };
+
+
+    if (!userFields.firstName) {
+      newUserFieldsErrors.firstNameError = "First name is required";
+      isValid = true;
+    } else {
+      newUserFieldsErrors.firstNameError = "";
+    }
+
+    if (!userFields.lastName) {
+      newUserFieldsErrors.lastNameError = "Last name is required";
+      isValid = true;
+    } else {
+      newUserFieldsErrors.lastNameError = "";
+    }
+
+    if (!userFields.phoneNum) {
+      newUserFieldsErrors.firstNameError = "Phone number is required";
+      isValid = true;
+    } else {
+      newUserFieldsErrors.phoneNumError = "";
+    }
+
+    if (!userFields.password) {
+      newUserFieldsErrors.firstNameError = "Password is required";
+      isValid = true;
+    } else if (userFields.password.length < 6) {
+      newUserFieldsErrors.firstNameError = "Password should be 6 character long";
+      isValid = true;
+    }
+    else {
+      newUserFieldsErrors.firstNameError = "";
+    }
+
+    
   }
   // Handling Fields
   const fieldChange = (e) => {
@@ -30,8 +68,25 @@ const CreateUser = () => {
   }
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input type='text'/>
+      <form className='form-create-user' onSubmit={handleSubmit}>
+
+        <label>First Name</label>
+        <input type='text' name='firstName' value={userFields.firstName} onChange={fieldChange} />
+        <span className='error-message'>{userFieldsError.firstNameError}</span>
+
+        <label>Last Name</label>
+        <input type='text' name='lastName' value={userFields.lastName} onChange={fieldChange} />
+        <span className='error-message'>{userFieldsError.lastNameError}</span>
+
+        <label>Phone No</label>
+        <input type='text' name='phoneNum' value={userFields.phoneNum} onChange={fieldChange} />
+        <span className='error-message'>{userFieldsError.phoneNumError}</span>
+
+        <label>Password</label>
+        <input type='text' name='password' value={userFields.password} onChange={fieldChange} />
+        <span className='error-message'>{userFieldsError.passwordError}</span>
+
+        <button className='add-user'>Add User</button>
       </form>
     </div>
   )
